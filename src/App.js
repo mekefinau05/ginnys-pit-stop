843cc74fe2f482ce3403c96226b3e80eeb01003b
Meke Finau <lonifinau.09@gmail.com>
2022-05-27 19:51:08 -0600

    ginnys pit stop

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import NavLog from './NavLog';
+import Login from './Login';
+import Register from './Register';
+
+function App() {
+  return (
+    <div>
+      <NavLog />
+      <Login />
+      <Register />
+      <h1 className='App-header'>Ginny's Pit Stop</h1>
+      <h2>Soda Place</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/NavLog.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './NavLog.css';
+import { Link } from 'react-router-dom';
+
+function NavLog() {
+  return (
+    <nav className='nav'>
+      <Link className='link' to='/'>
+        Home
+      </Link>{' '}
+      |{' '}
+      <Link className='link' to='/login'>
+        Login
+      </Link>{' '}
+      |{' '}
+      <Link className='link' to='/register'>
+        Signup
+      </Link>
+    </nav>
+  );
+}
+
+export default NavLog;
