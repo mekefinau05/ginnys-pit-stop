278a021fce32a8d447eb8bdc34fcc21e7b27f989
Meke Finau <lonifinau.09@gmail.com>
2022-07-18 21:53:45 -0600

    login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import NavLog from './NavLog';
-// import Login from './Login';
+// import NavLog from './NavLog';
+import Login from './Login';
 // import Register from './Register';
 
 function App() {
   return (
     <div className='App'>
-      <NavLog />
+      <Login />
       <h1 className='App-header'>Ginny's Pit Stop</h1>
       <h2>Soda Place</h2>
     </div>
--- src/Login.js
+++ src/Login.js
@@ -3,8 +3,11 @@ import React from 'react';
 function Login() {
   return (
     <div>
-      <h1>Login</h1>;<h4>Username</h4>
-      <input></input>
+      <h1>Login</h1>
+      <h4>Username</h4>
+      <input placeholder='username'></input>
+      <h4>Password</h4>
+      <input placeholder='password'></input>
     </div>
   );
 }
